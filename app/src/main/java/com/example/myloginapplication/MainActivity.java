f66040b8055be804a439843723f0aef50df7a2b0
rhansel3 <rhansel3@gatech.edu>
2023-09-06 16:40:13 -0400

    first commit

---
+++ app/src/main/java/com/example/myloginapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myloginapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username = findViewById(R.id.username);
+        TextView password = findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
