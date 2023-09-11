41a243707c42ef7a80a3dc71450d3cbca7af5417
rhansel3 <rhansel3@gatech.edu>
2023-09-11 18:50:52 -0400

    When the user clicks login it will redirect to another page

--- app/src/main/java/com/example/myloginapplication/MainActivity.java
+++ app/src/main/java/com/example/myloginapplication/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.myloginapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,12 +36,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    openActivity2();
                 } else {
                     Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 
     public void GetTextFromSQL(View v)
